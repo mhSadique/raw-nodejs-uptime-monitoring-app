2d463e650cddc7e195b244f1f5729f7ff27437f5
Sadique <mhsadique1307@gmail.com>
2022-03-30 09:10:18 +0600

    check CRUD done

--- routes.js
+++ routes.js
@@ -8,11 +8,13 @@
 const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
 const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler');
 const { userHandler } = require('./handlers/routeHandlers/userHandler');
+const { checkHandler } = require('./handlers/routeHandlers/checkHandler');
 // below object holds the route names and their corresponding functions that will be called when the routes will be hit
 const routes = {
     'sample': sampleHandler,
     'user': userHandler,
-    'token': tokenHandler
+    'token': tokenHandler,
+    'check': checkHandler
 };
 
 module.exports = routes;
